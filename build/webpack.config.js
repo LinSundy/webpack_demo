cddf6e82ca6597aa2f1445e745c97bdc060fefb2
LinSundy <403514753@qq.com>
2019-09-10 20:57:49 +0800

    增加css压缩， js压缩， 以及新建模板插件

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const devMode = process.env.NODE_ENV !== 'production';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js', // 相对路径是最外层
@@ -47,12 +49,29 @@ module.exports = {
     ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'AICODER 全栈线下实习', // 默认值：Webpack App
+      filename: 'main.html', // 默认值： 'index.html'
+      template: path.resolve(__dirname, '../src/index.html'),
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeAttributeQuotes: true // 移除属性的引号
+      }
+    }),
     new MiniCssExtractPlugin({
       filename: devMode ? '[name].css' : '[name].[hash].css', // 设置最终输出的文件名
       chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
     })
   ],
   optimization: {
-    minimizer: [new OptimizeCSSAssetsPlugin({})] // 只有在生产模式下才会起作用
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ] // 只有在生产模式下才会起作用
   }
 }
