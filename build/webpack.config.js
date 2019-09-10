ccdf48ee18f25c29a8055723e8d95236859b02c1
chelin <chelin>
2019-09-10 17:38:45 +0800

    1. 增加了css压缩插件 2. css提取插件 3. css自动补全前缀插件 4. less-loader的支持

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,27 +1,58 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    entry: './src/index.js', // 想对路径是最外层
-    mode: 'development',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, '../dist') // 项目目录下的dist
-    },
-    module: {
-        rules: [
+  entry: './src/index.js', // 相对路径是最外层
+  // mode: 'development',
+  mode: 'production',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '../dist') // 项目目录下的dist
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(le|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          // devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          MiniCssExtractPlugin.loader,
+          { 
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
           {
-            test: /\.(le|c)ss$/,
-            exclude: /node_modules/,
-            use: [
-              'style-loader',
-              {loader: 'css-loader'},
-              {
-                loader: 'less-loader',
-                options: {
-                  noIeCompat: true
-                }
-              }]
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: true,
+              plugins: loader => [
+                require('autoprefixer') // 添加前缀
+              ]
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              noIeCompat: true,
+              sourceMap: true
+            }
           }
         ]
-    }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css', // 设置最终输出的文件名
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+    })
+  ],
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})] // 只有在生产模式下才会起作用
+  }
 }
