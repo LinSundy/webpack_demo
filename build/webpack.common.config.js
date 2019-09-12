4a341fef4c9c6117523540013bb07fba14b51b69
chelin <chelin>
2019-09-12 14:01:33 +0800

    增加babel转义

--- build/webpack.common.config.js
+++ build/webpack.common.config.js
@@ -6,16 +6,13 @@ module.exports = {
     entry: './src/index.js',
     module: {
         rules: [
-            // {
-            //     test: /\.m?js$/,
-            //     exclude: /(node_modules|bower_components)/,
-            //     use: {
-            //         loader: 'babel-loader',
-            //         options: {
-            //             presets: ['@babel/preset-env']
-            //         }
-            //     }
-            // }
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
         ]
     },
     plugins: [
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import './assert/css/index.css';
 import './assert/less/index.less';
 
 function createElement() {
-    // let dom = document.createElement('div');
-    // dom.innerHTML = _.join(['hello', 'world', '你好', '世界'], '-');
-    // dom.classList.add('box');
-    // return dom;
+    let dom = document.createElement('div');
+    dom.innerHTML = _.join(['hello', 'world', '你好', '世界'], '-');
+    dom.classList.add('box');
+    return dom;
 }
 
 let divDom = createElement();
