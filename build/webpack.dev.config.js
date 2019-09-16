89b7d0f80f16f604dc03eb4ca7b3cb4c34964283
chelin <chelin>
2019-09-16 11:27:21 +0800

    去除html lang=en

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -18,14 +18,15 @@ let devConfig = {
         host: "0.0.0.0",
         open: true,
         openPage: "main.html",
+        progress: true,
         overlay: {  // 出现错误或者警告的时候，是否覆盖页面线上错误消息。
             warnings: true,
             errors: true
         },
         watchOptions: { // 监视文件相关的控制选项
-            poll: true,   // webpack 使用文件系统(file system)获取文件改动的通知。在某些情况下，不会正常工作。例如，当使用 Network File System (NFS) 时。Vagrant 也有很多问题。在这些情况下，请使用轮询. poll: true。当然 poll也可以设置成毫秒数，比如：  poll: 1000
+            // poll: true,   // webpack 使用文件系统(file system)获取文件改动的通知。在某些情况下，不会正常工作。例如，当使用 Network File System (NFS) 时。Vagrant 也有很多问题。在这些情况下，请使用轮询. poll: true。当然 poll也可以设置成毫秒数，比如：  poll: 1000
             ignored: /node_modules/, // 忽略监控的文件夹，正则
-            aggregateTimeout: 300 // 默认值，当第一个文件更改，会在重新构建前增加延迟
+            // aggregateTimeout: 300 // 默认值，当第一个文件更改，会在重新构建前增加延迟
         }
     },
     devtool: "inline-source-map",
