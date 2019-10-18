e9c3b01d589bbe57c85251bdec0a6b3a88aad046
chelin <chelin>
2019-10-18 14:53:54 +0800

    修复代码格式

--- src/pages/page2/index.vue
+++ src/pages/page2/index.vue
@@ -1,19 +1,17 @@
 <template>
-    <div>
-        <Hello></Hello>
-        <h3>可以拖拽并且改变大小的弹出框</h3>
-        <div>
-            VueDraggableResizable 2
-        </div>
+  <div>
+    <Hello></Hello>
+    <h3>可以拖拽并且改变大小的弹出框</h3>
+    <div>VueDraggableResizable 2</div>
         <div>{{aa}}</div>
-    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Hello from "@/components/hello.vue";
-import Vue from 'vue';
+import Vue from "vue";
 
-const _ = require('lodash');
+const _ = require("lodash");
 
 export default Vue.extend({
   name: "page2",
